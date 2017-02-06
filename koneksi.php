a8c77bd94374cc0c85e2e39babde0893903c7c8d
Wahyu Budiman <wahyubudimanstt@gmail.com>
2017-02-06 18:32:56 +0700

    fg

--- koneksi.php
+++ koneksi.php
@@ -4,5 +4,5 @@ $h='mysql.idhostinger.com';
 $u='u617774943_dl';
 $p='rahasiaku';
 $db='u617774943_dl';
-mysqli_connect($h,$u,$p) or die('gagal konek');
+mysql_connect($h,$u,$p) or die('gagal konek');
 mysql_select_db($db) or die('eror db');

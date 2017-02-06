755d9104f70965a7f36533ac40a73de0a9bc41e5
Wahyu Budiman <wahyubudimanstt@gmail.com>
2017-02-06 18:30:30 +0700

    cxvc

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 
 $h='mysql.idhostinger.com';
 $u='u617774943_dl';
-$p='download-wahyudev';
+$p='rahasiaku';
 $db='u617774943_dl';
 mysqli_connect($h,$u,$p) or die('gagal konek');
 mysql_select_db($db) or die('eror db');
